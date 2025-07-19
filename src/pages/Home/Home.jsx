f5ad3989ac5ddcc6e7aaca81c9b6ed9e5e18eefc
Navnak <navnit.23bce11827@vitbhopal.ac.in>
2025-07-20 00:15:12 +0530

    mount projects and contact sections in UI

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import Hero from "./sections/Hero/Hero.jsx";
 import NavBar from "./sections/NavBar/NavBar.jsx";
 import About from "./sections/About/About.jsx";
 import Skills from "./sections/Skills/Skills.jsx";
+import Projects from "./sections/Projects/Projects.jsx";
+import Contact from "./sections/Contact/Contact.jsx";
 
 function Home(){
     return(
@@ -11,6 +13,8 @@ function Home(){
             <Hero/>
             <About/>
             <Skills/>
+            <Projects/>
+            <Contact/>
         </>
     )
 }
